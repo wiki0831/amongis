5384f17f6a2bce702592ac021c4482798b11e42b
wiki0831 <wiki0831@gmail.com>
2023-12-29 15:42:16 -0500

    refactor telem

--- database/player.go
+++ database/player.go
@@ -4,6 +4,8 @@ import (
 	"amongis/model"
 	"context"
 	"fmt"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func GetPlayerInfo(name string) (*model.PlayerModel, error) {
@@ -64,4 +66,28 @@ func GetAllPlayerInfo() ([]model.PlayerModel, error) {
 	}
 
 	return players, nil
+}
+
+func CreatePlayerTelemetry(user model.PlayerModel) error {
+	query := `
+			INSERT INTO player 
+				(name, role, room, status, location, created_at) 
+			VALUES 
+				(@userName, @userRole, @userRoom, @userStatus, ST_GeomFromWKB(@userLocation,4326), @userTimestamp)`
+
+	args := pgx.NamedArgs{
+		"userName":      user.Name,
+		"userRole":      user.Role,
+		"userRoom":      user.Room,
+		"userStatus":    user.Status,
+		"userLocation":  user.Location.AsBinary(),
+		"userTimestamp": user.CreatedAt,
+	}
+
+	_, err := DB.Exec(context.Background(), query, args)
+	if err != nil {
+		return fmt.Errorf("unable to insert row: %w", err)
+	}
+
+	return nil
 }
